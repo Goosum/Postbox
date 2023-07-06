8225d2e02609d20a75daa27c189bc40578d65faf
Goosums <coopercooke@yahoo.com>
2023-07-06 10:05:57 -0400

    Fixed postbox texture

--- src/main/java/io/github/goosum/postbox/block/PostboxBlock.java
+++ src/main/java/io/github/goosum/postbox/block/PostboxBlock.java
@@ -2,15 +2,25 @@ package io.github.goosum.postbox.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import org.jetbrains.annotations.Nullable;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
 
-public class PostboxBlock extends Block {
+public class PostboxBlock extends HorizontalFacingBlock {
+
+	public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
 
 	public PostboxBlock(Settings settings) {
 		super(settings);
@@ -28,4 +38,24 @@ public class PostboxBlock extends Block {
 		return SHAPE;
 	}
 
+	@Nullable
+	@Override
+	public BlockState getPlacementState(ItemPlacementContext ctx) {
+		return this.getDefaultState().with(FACING, ctx.getPlayerFacing());
+	}
+
+	@Override
+	public BlockState rotate(BlockState state, BlockRotation rotation) {
+		return state.with(FACING, rotation.rotate(state.get(FACING)));
+	}
+
+	@Override
+	public BlockState mirror(BlockState state, BlockMirror mirror) {
+		return state.rotate(mirror.getRotation(state.get(FACING)));
+	}
+
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		builder.add(FACING);
+	}
 }
